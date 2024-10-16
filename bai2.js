3b775896b8a009efae6329f59c0f2736a9f8ff6f
John Doe <john.doe@example.com>
2024-10-16 23:23:35 +0700

    xaa

--- bai2.js
+++ bai2.js
@@ -1,4 +1,30 @@
-var a=1
-a+=1
-console.log(a)
-//muon co giai thich 
+// const a = [1, 2, 3, 4]; // Example array
+// const age = 25; // Define the 'age' variable
+
+// const ad = a.every((a, index) => {
+//   console.log(a, index);
+//   return a + age + 1; // This now has access to 'age'
+// });
+
+
+const b=[
+    {
+        name:'xuandat',
+        coin:1000
+    },
+    {
+        name: 'hieu',
+        coin:2000
+    },
+     {
+        name: 'hieu2',
+        coin:3000
+    }
+]
+
+const d = b.find((ba, index) => {
+    console.log(index)
+    return ba.coin === 1000
+})
+
+console.log(d)
